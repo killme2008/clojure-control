dc12e7f4c4524794d5e5a4428839667c8911112c
Hong Jiang <hong.jiang@onycloud.com>
2011-08-05 13:36:45 +0800

    Make scp a function. It does not need to be a macro.

--- src/control/core.clj
+++ src/control/core.clj
@@ -58,12 +58,12 @@
   (exec host user ["ssh" (ssh-client host user) cmd]))
 
 
-(defmacro scp
+(defn scp
   [host user files remoteDir]
-  `(do (log-with-tag ~host "scp"
-         (join " " (concat ~files [ " ==> " ~remoteDir])))
-       (exec ~host ~user ["scp" ~@files (str (ssh-client ~host ~user) ":"
-                                             ~remoteDir)])))
+  (log-with-tag host "scp"
+    (join " " (concat files [ " ==> " remoteDir])))
+  (exec host user
+        (concat ["scp"] files [(str (ssh-client host user) ":" remoteDir)])))
 
 
 
