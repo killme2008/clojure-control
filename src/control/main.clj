3aead9ae51f73df855e33d856bd4989e11124b7e
Sun Ning <classicning@gmail.com>
2011-08-07 11:19:46 +0800

    add shell wrapper

---
+++ src/control/main.clj
@@ -0,0 +1,17 @@
+(ns control.main
+  (:use [control.core])
+  (:use [control.commands]))
+
+(defn- load-control-file [filename]
+  (try 
+    (binding [*ns* (the-ns 'control.main)]
+      (load-file filename))
+  (catch java.io.FileNotFoundException e (println "control file not found."))))
+
+(defn -main
+  [file & args]
+  (do
+    (load-control-file file)
+    (println args)
+    (do-begin args)))
+
