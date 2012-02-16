495452f6f56f4a54f38db4030282d50d430761ec
dennis zhuang <killme2008@gmail.com>
2012-02-16 16:50:03 +0800

    Fixed unit test failed

--- test/control/test/commands.clj
+++ test/control/test/commands.clj
@@ -10,47 +10,47 @@
   (is (= "export PATH=/home/sun/bin:$PATH; " (path "/home/sun/bin"))))
 
 (deftest test-env-run
-  (is (= "JAVA_OPTS=-XMaxPermSize=128m java -version; "
+  (is (= "JAVA_OPTS=-XMaxPermSize=128m java -version ; "
          (env "JAVA_OPTS" "-XMaxPermSize=128m"
               (run "java -version")))))
 
 (deftest test-cd-run
-  (is (= "cd /home/sun; ls; "
+  (is (= "cd /home/sun; ls ; "
          (cd "/home/sun"
              (run "ls")))))
 
 (deftest test-cd-prefix-run
-  (is (= "cd /home/sun; source ~/.bash_profile && ls; "
+  (is (= "cd /home/sun; source ~/.bash_profile && ls ; "
          (cd "/home/sun"
              (prefix "source ~/.bash_profile"
                      (run "ls"))))))
 
 (deftest test-sudo
-  (is (= "sudo service tomcat start; "
+  (is (= "sudo service tomcat start ; "
          (sudo "service tomcat start"))))
 
 (deftest test-append
-  (is (= "echo 'hello world' >> test.txt"
+  (is (= "echo 'hello world' >> test.txt ; "
          (append "test.txt" "hello world")))
-  (is (= "echo 'hello world' | sudo tee -a test.txt"
+  (is (= "echo 'hello world' | sudo tee -a test.txt ; "
          (append "test.txt" "hello world" :sudo true))))
 
 
 (deftest test-sed
-  (is (= "sed -i.back -r -e \"3 s/hello/world/g\" test"
+  (is (= "sed -i.back -r -e \"3 s/hello/world/g\" test ; "
          (sed "test" "hello" "world" :limit 3 :backup ".back")))
-  (is (= "sudo sed -i.bak -r -e \" s/hello/world/g\" test; "
+  (is (= "sudo sed -i.bak -r -e \" s/hello/world/g\" test ; "
          (sed "test" "hello" "world" :sudo true))))
 
 (deftest test-comm
-  (is   (= "sed -i.bak -r -e \" s/hello\\sworld/#&/g\" test"
+  (is   (= "sed -i.bak -r -e \" s/hello\\sworld/#&/g\" test ; "
            (comm "test" "hello\\sworld")))
-  (is    (= "sed -i.bak -r -e \" s/hello\\sworld/;&/g\" test"
+  (is    (= "sed -i.bak -r -e \" s/hello\\sworld/;&/g\" test ; "
             (comm "test" "hello\\sworld" :char ";"))))
 
 (deftest test-uncomm
-  (is   (= "sed -i.bak -r -e \" s/#(hello\\sworld)/\\1/g\" test"
+  (is   (= "sed -i.bak -r -e \" s/#(hello\\sworld)/\\1/g\" test ; "
            (uncomm "test" "hello\\sworld")))
-  (is    (= "sed -i.bak -r -e \" s/;(hello\\sworld)/\\1/g\" test"
+  (is    (= "sed -i.bak -r -e \" s/;(hello\\sworld)/\\1/g\" test ; "
             (uncomm "test" "hello\\sworld" :char ";"))))
 
