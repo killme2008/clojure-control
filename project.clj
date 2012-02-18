654a9e1bef2bf95160dfbad3abcdb820d9708d7a
dennis zhuang <killme2008@gmail.com>
2012-02-18 21:20:41 +0800

    Relase 0.3.1

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject control "0.3.1-SNAPSHOT"
+(defproject control "0.3.1"
   :description "A clojure DSL for system admin and deployment with many remote machines"
   :url "https://github.com/killme2008/clojure-control"
   :author "dennis zhuang(killme2008@gmail.com)"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[lein-exec "0.1"]]
   :shell-wrapper {:bin "bin/clojure-control"
                   :main control.main})
