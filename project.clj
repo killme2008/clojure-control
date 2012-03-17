1d1aeaa31eadbfec202d284760f17a5ac6bb9d2f
dennis zhuang <killme2008@gmail.com>
2012-03-17 18:46:32 +0800

    Change version to 0.3.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject control "0.3.1-SNAPSHOT"
+(defproject control "0.3.2-SNAPSHOT"
   :description "A clojure DSL for system admin and deployment with many remote machines"
   :url "https://github.com/killme2008/clojure-control"
   :author "dennis zhuang(killme2008@gmail.com)"
