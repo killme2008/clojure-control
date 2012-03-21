f86b17fb86c21a1c41878cdc73ee109f79cd9a56
dennis zhuang <killme2008@gmail.com>
2012-03-21 21:49:15 +0800

    Adds codx,autodoc and marginalia dev dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/killme2008/clojure-control"
   :author "dennis zhuang(killme2008@gmail.com)"
   :dependencies [[org.clojure/clojure "1.3.0"] [org.clojure/tools.cli "0.2.1"]]
-  :dev-dependencies [[lein-exec "0.1"]]
+  :dev-dependencies [[lein-exec "0.1"]
+                     [lein-marginalia "0.7.0"]
+                     [lein-autodoc "0.9.0"]
+                     [codox "0.5.0"]]
   :shell-wrapper {:bin "bin/clojure-control"
                   :main control.main})
